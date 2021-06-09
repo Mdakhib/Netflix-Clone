a7a02d05ba199169fcf93b7b1ede8814e8a3cd9c
Mdakhib <mohamedakhib@gmail.com>
2021-06-09 15:54:58 +0530

    opt-form added with the faqs.js which is in the same section by using compound components

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { Container, Input, Button, Text } from "./styles/opt-form";
 
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import faqsData from "../fixtures/faqs.json";
 import { Accordion } from "../components";
+import OptForm from "../components/opt-form";
 
 export function FaqsContainer() {
   return (
@@ -12,6 +13,14 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body} </Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
