707bf744a5100dcef805b86578c3d70deb7b7c9d
Mdakhib <mohamedakhib@gmail.com>
2021-06-09 15:46:25 +0530

    opt-form folder created and initial set-up added by using compound components

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div``;
+
+export const Input = styled.input``;
+
+export const Button = styled.button``;
+
+export const Text = styled.p``;
