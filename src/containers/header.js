988ab74386e4003987389372c646f016ca433f9f
Mdakhib <mohamedakhib@gmail.com>
2021-06-09 22:48:53 +0530

    header folder and added code by using roters and compound components

---
+++ src/containers/header.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
+
+export function HeaderContainer({ Children }) {
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+      </Header.Frame>
+      {Children}
+    </Header>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer></HeaderContainer>
+
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
