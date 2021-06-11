ccb46b3fb16be2de52247ef6d2fa8359f07a73e5
Mdakhib <mohamedakhib@gmail.com>
2021-06-11 14:52:20 +0530

    config data added by firebase website

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,14 @@ import "firebase/auth";
 //we need to somehow seed the database
 
 //we need to config here
-const config = {};
+const config = {
+  apiKey: "AIzaSyC4_r3STP1qc2Ne-_oVykZlc9zXjoxMbyo",
+  authDomain: "netflix-aee7f.firebaseapp.com",
+  projectId: "netflix-aee7f",
+  storageBucket: "netflix-aee7f.appspot.com",
+  messagingSenderId: "60622029233",
+  appId: "1:60622029233:web:6119653be3f2c0ba2c3d90",
+};
 
 const firebase = Firebase.initializeApp(config);
 
