e3e803a0ffa2c0cf90492da5f20f844219894bae
Mdakhib <mohamedakhib@gmail.com>
2021-06-11 12:45:49 +0530

    feature section added in the home page which is done by compound components and styled component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Jumbotron } from "./jumbotron";
 export { default as Footer } from "./footer";
 export { default as OptForm } from "./opt-form";
 export { default as Header } from "./header";
+export { default as Feature } from "./feature";
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,20 +4,27 @@ import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { OptForm } from "../components";
+import { Feature } from "../components";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email Address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>
-            Ready to watch? Enter your email to create or restart your
-            membership
-          </OptForm.Text>
-        </OptForm>
+        <Feature>
+          <Feature.Title>Unlimited films,TV Shows and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time
+          </Feature.SubTitle>
+            <OptForm>
+              <OptForm.Input placeholder="Email Address" />
+              <OptForm.Button>Try it now</OptForm.Button>
+              <OptForm.Break />
+              <OptForm.Text>
+                Ready to watch? Enter your email to create or restart your
+                membership
+              </OptForm.Text>
+            </OptForm>
+        </Feature>
       </HeaderContainer>
 
       <JumbotronContainer />
