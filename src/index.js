627c79a07253d25dcf30bcf5bebacdd63556adfd
Mdakhib <mohamedakhib@gmail.com>
2021-06-11 15:06:55 +0530

    by importing seed.js in firebase.prod.js and firebase in index.js all data will be copied to the firebase collection in website

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { render } from "react-dom";
 import App from "./app";
 import 'normalize.css'
 import { GlobalStyles } from "./global-styles";
-
+// eslint-disable-next-line no-unused-vars
+import { firebase } from "./lib/firebase.prod";
 render(
   <>
     <GlobalStyles /> <App />
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,6 +1,7 @@
 import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import { seedDatabase } from "../seed";
 
 //we need to somehow seed the database
 
@@ -15,5 +16,7 @@ const config = {
 };
 
 const firebase = Firebase.initializeApp(config);
+
+seedDatabase(firebase)
 
 export { firebase };
