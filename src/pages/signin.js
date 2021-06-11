a7d07b35602740819e11bb8453b161bd3d2653c1
Mdakhib <mohamedakhib@gmail.com>
2021-06-11 18:44:31 +0530

    form structure created by using compond components

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { isValidElement, useState } from "react";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 import { Form } from "../components";
@@ -16,7 +17,26 @@ export default function Signin() {
   return (
     <>
       <HeaderContainer>
-        
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error} </Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              autoComplete='off'
+              value={emailAddress}
+              onChange={({ target }) => setemailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              autoComplete='off'
+              value={password}
+              onChange={({ target }) => setpassword(target.value)}
+            />
+            <Form.Submit disable={isInvalid} type='submit' >Sign In</Form.Submit>
+          </Form.Base>
+        </Form>
       </HeaderContainer>
       <FooterContainer />;
     </>
