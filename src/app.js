110f84e4027c692506496ef217d3a9b37bb0b6e9
Mdakhib <mohamedakhib@gmail.com>
2021-06-12 18:38:52 +0530

    by using helpers routes.js the signin and signup is protected by taking their props in into the routers

--- src/app.js
+++ src/app.js
@@ -1,19 +1,43 @@
+
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect } from "./helpers/routes";
 export default function App() {
+  const user  =  {};
   return (
     <Router>
       <Route exact path="/browse">
         <Browse />
       </Route>
-      <Route exact path="/signin">
+
+
+
+      
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
-      <Route exact path="/signup">
+      </IsUserRedirect>
+
+
+      
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
+      </IsUserRedirect>
+
+
+
+
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,6 +1,6 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-
 
 export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
   return (
