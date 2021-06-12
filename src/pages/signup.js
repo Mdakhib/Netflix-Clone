668972b2978b008be6a028c58b2ec402dba3b513
Mdakhib <mohamedakhib@gmail.com>
2021-06-12 13:06:07 +0530

    signup code copied from signin file and updated

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,74 @@
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signup() {
-  return <h1>Hello this is SignUp page</h1>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  
+  const isInvalid = firstName === "" || password === "" || emailAddress === "";
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+
+
+    //do firebase stuff
+
+}
+
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error} </Form.Error>}
+
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              autoComplete="off"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email or Phone Number"
+              autoComplete="off"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              type="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+          <Form.Text>
+            Already a user?  <Form.Link to="/signin">Sign in now</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />;
+    </>
+  );
 }
