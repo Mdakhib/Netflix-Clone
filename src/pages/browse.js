640110dca6149f0990de16cc950574e4069df5d7
Mdakhib <mohamedakhib@gmail.com>
2021-06-13 14:19:59 +0530

    used  firebase  to access the collection, like target collection will be either films r series then we get that collection in which we get buch of documents by using map by taking unique keys with that docId after that it will set the content in setContent and returns the target collection like films r series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { useContent } from "../hooks";
 
 export default function Browse() {
+  //we need the series and the films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  console.log(series);
+  console.log(films);
+  //we need the slides
+  //pass it to the browse container
+
   return <h1>Hello from the Browse page</h1>;
 }
