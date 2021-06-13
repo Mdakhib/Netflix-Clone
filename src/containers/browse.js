e6d94e6dbbb02994ab52066c66d7d423eb836a92
Mdakhib <mohamedakhib@gmail.com>
2021-06-13 18:29:09 +0530

    added spinner timeout using useEffect and  added onClick event on Profile.user in which we can get the name and the user picture

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [[profile.displayName]]);
+
+
   return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
