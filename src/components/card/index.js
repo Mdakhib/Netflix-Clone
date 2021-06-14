e93a9e35ad68ec4fe3f8e48028c3815fba86b322
Mdakhib <mohamedakhib@gmail.com>
2021-06-14 14:35:29 +0530

    useContext is created for the feature.It is the card in which it shows the maturity like 13+,16+ r 18+

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+import { Container } from "./styles/card";
+
+export const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children} </Container>
+    </FeatureContext.Provider>
+  );
+}
